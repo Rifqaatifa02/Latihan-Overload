f199bffd139796f952022e0655bf733f52ccc514
Rifqaatifa02 <127828463+Rifqaatifa02@users.noreply.github.com>
2023-04-10 03:48:56 +0700

    Add files via upload

---
+++ Numbers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Internal;
+
+namespace Overload
+{
+    public class Numbers
+    {
+        public int cariNilaiMin(int a, int b)
+        {
+            int min = 0;
+            if (a < b)
+            {
+                Console.WriteLine("Nilai minimum dari kedua nilai adalah = {0}", min = a);
+                Console.ReadKey();
+            }
+            else if (b < a)
+            {
+                Console.WriteLine("Nilai minimum dari kedua nilai adalah = {0}", min = b);
+                Console.ReadKey();
+            }
+
+            return min;
+        }
+        public int cariNilaiMax(int a, int b)
+        {
+            int max = 0;
+            if (a > b)
+            {
+                Console.WriteLine("Nilai maksimum dari kedua nilai adalah = {0}", max = a);
+                Console.ReadKey();
+            }
+            else if (b > a)
+            {
+                Console.WriteLine("Nilai maksimum dari kedua nilai adalah = {0}", max = b);
+                Console.ReadKey();
+            }
+
+            return max;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Overload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers Nilai = new Numbers();
+            Nilai.cariNilaiMin(40, 20);
+            Nilai.cariNilaiMax(80, 30);
+        }
+    }
+}
